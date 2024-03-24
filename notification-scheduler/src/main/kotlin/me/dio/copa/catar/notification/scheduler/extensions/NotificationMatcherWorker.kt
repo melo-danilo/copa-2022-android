891e503cc3fe129ee0ba367b2987b35ba2f4e1ac
Danilo Melo <danilofr.melo@gmail.com>
2024-03-24 16:29:25 -0300

    projeto funcionando

--- notification-scheduler/src/main/kotlin/me/dio/copa/catar/notification/scheduler/extensions/NotificationMatcherWorker.kt
+++ notification-scheduler/src/main/kotlin/me/dio/copa/catar/notification/scheduler/extensions/NotificationMatcherWorker.kt
@@ -13,18 +13,18 @@ import me.dio.copa.catar.domain.model.MatchDomain
 import java.time.Duration
 import java.time.LocalDateTime
 
-private const val NOTIFICATION_TITLE_KEY = "notification_title"
-private const val NOTIFICATION_CONTENT_KEY = "notification_content"
+private const val NOTIFICATION_TITLE_KEY = "NOTIFICATION_TITLE_KEY"
+private const val NOTIFICATION_CONTENT_KEY = "NOTIFICATION_CONTENT_KEY"
 
 class NotificationMatcherWorker(
-    private val context: Context,workerParams: WorkerParameters
+    private val context: Context,
+    workerParams: WorkerParameters,
 ) : Worker(context, workerParams) {
-
     override fun doWork(): Result {
         val title = inputData.getString(NOTIFICATION_TITLE_KEY)
-            ?: throw IllegalArgumentException("Title is missing")
+            ?: throw IllegalArgumentException("title is required")
         val content = inputData.getString(NOTIFICATION_CONTENT_KEY)
-            ?: throw IllegalArgumentException("Content is missing")
+            ?: throw IllegalArgumentException("content is required")
 
         context.showNotification(title, content)
 
@@ -33,13 +33,12 @@ class NotificationMatcherWorker(
 
     companion object {
         fun start(context: Context, match: MatchDomain) {
-
             val (id, _, _, team1, team2, matchDate) = match
 
             val initialDelay = Duration.between(LocalDateTime.now(), matchDate).minusMinutes(5)
             val inputData = workDataOf(
-                NOTIFICATION_TITLE_KEY to "Se prepara! O Jogo está chegando!",
-                NOTIFICATION_CONTENT_KEY to "Hoje têm ${team1.displayName} x ${team2.displayName}"
+                NOTIFICATION_TITLE_KEY to "Se prepare que o jogo vai começar",
+                NOTIFICATION_CONTENT_KEY to "Hoje tem ${team1.flag} vs ${team2.flag}",
             )
 
             WorkManager.getInstance(context)
@@ -50,17 +49,15 @@ class NotificationMatcherWorker(
                 )
         }
 
+        fun cancel(context: Context, match: MatchDomain) {
+            WorkManager.getInstance(context)
+                .cancelUniqueWork(match.id)
+        }
+
         private fun createRequest(initialDelay: Duration, inputData: Data): OneTimeWorkRequest =
             OneTimeWorkRequestBuilder<NotificationMatcherWorker>()
                 .setInitialDelay(initialDelay)
                 .setInputData(inputData)
                 .build()
-
-        fun cancel(context: Context, match: MatchDomain) {
-            WorkManager.getInstance(context)
-                .cancelUniqueWork(match.id)
-        }
     }
-
-
 }
